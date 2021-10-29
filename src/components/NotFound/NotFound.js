a2ae607011e1a1cd2160471f72b6c7b586b85534
raiyan <raiyan5764@gmail.com>
2021-10-30 01:08:01 +0600

    not found added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,41 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { BsArrowLeft } from "react-icons/bs";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1 className="text-center text-danger">404 page not found</h1>
-    </div>
+    <section className="page-not">
+      <Container>
+        <Row className="py-4">
+          <Col xs={12} lg={12} md={12}>
+            <div className="back-btn">
+              <button onClick={() => window.history.back()}>
+                {" "}
+                <BsArrowLeft size={20} className="me-2" /> Back
+              </button>
+            </div>
+          </Col>
+          <Col xs={12} lg={12} md={12}>
+            <div className="not-found-image d-flex justify-content-center align-items-center flex-column">
+              <div>
+                <img
+                  src="https://i.ibb.co/F5P8q4t/404-Error-Page-not-Found-with-people-connecting-a-plug-pana.png"
+                  alt=""
+                  width="500px"
+                />
+              </div>
+              <Link className="text-decoration-none" to="/home">
+                <button className="d-block btn btn-outline-dark m-auto py-2 px-5">
+                  Home
+                </button>
+              </Link>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
